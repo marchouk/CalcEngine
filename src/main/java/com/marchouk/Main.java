4f5c0e657fbcfbac3e10fc8314aff1c712153ade
MARCHOUK <el-mehdi.marchouk@capgemini.com>
2024-02-16 11:47:40 +0100

    m4: Using block statements

--- src/main/java/com/marchouk/Main.java
+++ src/main/java/com/marchouk/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args)
     {
         double value1 = 100d;
-        double value2 = 50d;
-        double result;
+        double value2 = 0.0d;
+        double result = 0.0d;
         char opCode = 'd';
 
         if (opCode == 'a') {
@@ -15,8 +15,11 @@ public class Main {
         } else if (opCode == 'm') {
             result = value1 * value2;
         } else if (opCode == 'd') {
-            result = value1 / value2;
+            if (value2 != 0) {
+                result = value1 / value2;
+            }
         } else {
+            System.out.println("Invalid opCode: " + opCode);
             result = 0.0d;
         }
 
